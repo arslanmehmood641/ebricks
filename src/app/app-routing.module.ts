bd891e0473295bde55edc6ee00e18637a5ab7163
arslan <arslanmehmood641@gmail.com>
2019-07-23 08:36:36 +0500

    7-23-2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { AuthTabsComponent } from "./components/auth-tabs/auth-tabs.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { QueueComponent } from './components/queue/queue.component';
+import { PeopleComponent } from './components/people/people.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { FollowingsComponent } from './components/followings/followings.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AuthTabsComponent },
+  { path: "queue", component: QueueComponent },
+  {path:"people",component:PeopleComponent},
+  {path:"chat/:name",component:ChatComponent},
+  {path:"followers",component:FollowersComponent},
+  {path:"followings",component:FollowingsComponent},
+  {path:"notifications",component:NotificationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthTabsComponent } from './components/auth-tabs/auth-tabs.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import {UserService} from './services/user.service';
+import { QueueComponent } from './components/queue/queue.component';
+import { PeopleComponent } from './components/people/people.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MessageComponent } from './components/message/message.component'
+import { MessageService } from './services/message.service';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FriendsService } from './services/friends.service';
+import { FollowersComponent } from './components/followers/followers.component';
+import { FollowingsComponent } from './components/followings/followings.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthTabsComponent,
+    SigninComponent,
+    SignupComponent,
+    QueueComponent,
+    PeopleComponent,
+    NavbarComponent,
+    ChatComponent,
+    MessageComponent,
+    SidebarComponent,
+    FollowersComponent,
+    FollowingsComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService,MessageService,FriendsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
